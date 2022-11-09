3d8c8871258fb0c1b1616b369011c19f91198e72
xuany1611 <xuany1611@gmail.com>
2022-11-09 10:36:21 +0700

    first commit

--- src/ss3_mang_va_phuong_thuc_trong_java/bai_tap/gop_mang.java
+++ src/ss3_mang_va_phuong_thuc_trong_java/bai_tap/gop_mang.java
@@ -1,7 +1,53 @@
 package ss3_mang_va_phuong_thuc_trong_java.bai_tap;
+import java.util.Scanner;
 
 public class gop_mang {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m;
 
+        do {
+            System.out.print("Nhập vào hàng: ");
+            m = Integer.parseInt(scanner.nextLine());
+            if (m <= 0) {
+                System.out.print("Số lượng hàng <0,xin nhập lại!");
+            }
+        } while (m <= 0);
+        int n;
+
+        do {
+            System.out.print("Nhập vào cột: ");
+            n = Integer.parseInt(scanner.nextLine());
+            if (n <= 0) {
+                System.out.print("Số lượng cột <0,xin nhập lại!");
+            }
+        } while (n <= 0);
+        int[][] arr = new int[m][n];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print("Nhập vào a[" + i + "][" + j + "]: ");
+                arr[i][j] = Integer.parseInt(scanner.nextLine());
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.printf("%2d", arr[i][j]);
+            }
+            System.out.println();
+        }
+        int max = arr[0][0];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                }
+            }
+        }
+        System.out.print("Phần tử lớn nhất là : " + max);
     }
 }
+    
+
---
+++ src/ss3_mang_va_phuong_thuc_trong_java/bai_tap/tim_phan_tu_lon_nhat_trong_mang_2_chieu.java
@@ -0,0 +1,5 @@
+package ss3_mang_va_phuong_thuc_trong_java.bai_tap;
+
+public class tim_phan_tu_lon_nhat_trong_mang_2_chieu {
+
+}
